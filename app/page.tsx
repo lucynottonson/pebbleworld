53f3a41cb9003d89bf63eae5a4bb5a2bf848df35
lucynottonson <lucynottonson@icloud.com>
2025-05-25 17:18:19 -0700

    wtf

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 export default function Home() {
   return (
     <div className="min-h-screen bg-[var(--background)] text-[var(--text)] font-serif">
-
       <main className="max-w-3xl mx-auto p-8 flex flex-col gap-10">
-        <section className="p-6 rounded bg-[var(--baccent)] text-[var(--accent)] shadow">
+        <section className="p-6 rounded bg-[var(--accent)] text-[var(--baccent)] shadow">
           <h2 className="text-2xl font-bold mb-2">Pebble World</h2>
           <p className="text-base">
             I am not quite finished. Maybe it will be like that forever. 
