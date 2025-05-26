50d1858d3cbd9f83bca20dccf8be87e694b2a023
lucynottonson <lucynottonson@icloud.com>
2025-05-25 17:01:05 -0700

    adjusted colors

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,11 +4,11 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="w-full px-6 py-4 flex justify-between items-center bg-[var(--background)] text-[var(--text)] shadow-md">
-      <div className="text-xl font-bold text-[var(--main)]">
+    <nav className="w-full px-6 py-4 flex justify-between items-center bg-[var(--main)] text-[var(--text)] shadow-md">
+      <div className="text-xl font-bold text-[var(--baccent)]">
         <Link href="/">PW</Link>
       </div>
-      <ul className="flex space-x-6 text-[var(--main)] ">
+      <ul className="flex space-x-6 text-[var(--baccent)] ">
         <li>
           <Link href="/pond" className="hover:text-[var(--accent)] transition-colors">
             Pond
