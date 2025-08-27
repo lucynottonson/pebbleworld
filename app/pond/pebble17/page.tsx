34102579b7dabc04097622c07023a86cd1920bb3
lucynottonson <lucynottonson@icloud.com>
2025-08-27 14:12:01 -0700

    bpo

--- app/pond/pebble17/page.tsx
+++ app/pond/pebble17/page.tsx
@@ -3,45 +3,15 @@ export default function PondPage() {
     <main className="min-h-screen flex flex-col items-center p-2 gap-10">
       <div className="w-full max-w-6xl flex flex-col items-center gap-2">
 
-        <div className="flex flex-col items-center gap-2 md:flex-row md:items-stretch w-full">
-          <div className="w-full border border-[var(--accent)] rounded-lg p-10 shadow-md flex flex-col items-center gap-4">
+        <div className="w-full border border-[var(--accent)] rounded-lg p-10 shadow-md flex flex-col items-center gap-4">
              <span className="text-[var(--baccent)] font-bold">Books</span>
-           </div>
-           <div className="w-full border border-[var(--accent)] rounded-lg p-10 shadow-md flex flex-col items-center gap-4">
-             <span className="text-[var(--baccent)] font-bold">here is a list of books that have critically shaped my existence as a thinking person. I am going to be very particular about what ends up on this list. I have enjoyed many books but not all of them have critically shaped my existence as a thinking person, obviously.</span>
-           </div>
         </div>
 
-        <div className="w-full border border-[var(--accent)] rounded-lg p-10 shadow-md flex flex-col items-center gap-4">
-             <span className="text-[var(--baccent)] font-bold">box</span>
-        </div>
-
-        <div className="w-full border border-[var(--accent)] rounded-lg p-10 shadow-md flex flex-col items-center gap-4">
-             <span className="text-[var(--baccent)] font-bold">box</span>
-        </div>
-
-        <div className="flex flex-col items-center gap-2 md:flex-row md:items-stretch w-full">
           <div className="w-full border border-[var(--accent)] rounded-lg p-10 shadow-md flex flex-col items-center gap-4">
-             <span className="text-[var(--baccent)] font-bold">i  know theres a better way to do this but im lazy </span>
-           </div>
-           <div className="w-full border border-[var(--accent)] rounded-lg p-10 shadow-md flex flex-col items-center gap-4">
-             <span className="text-[var(--baccent)] font-bold">plus this is a lot funnier </span>
-          </div>
+             <span className="text-[var(--baccent)] font-bold">here is a list of books that have critically shaped my existence as a thinking person. I am going to be very particular about what ends up on this list. I have enjoyed many books but not all of them have critically shaped my existence as a thinking person, obviously. Also they are not in chronological order or any order other than the one I put them in arbitrarily.
+</span>
         </div>
-
-         <div className="w-full border border-[var(--accent)] rounded-lg p-10 shadow-md flex flex-col items-center gap-4">
-             <span className="text-[var(--baccent)] font-bold">box</span>
-        </div>
-
-        <div className="flex flex-col items-center gap-2 md:flex-row md:items-stretch w-full">
-          <div className="w-full border border-[var(--accent)] rounded-lg p-10 shadow-md flex flex-col items-center gap-4">
-             <span className="text-[var(--baccent)] font-bold">box</span>
-           </div>
-           <div className="w-full border border-[var(--accent)] rounded-lg p-10 shadow-md flex flex-col items-center gap-4">
-             <span className="text-[var(--baccent)] font-bold">box</span>
-          </div>
-        </div>
-
+              
       </div>
     </main>
   );
