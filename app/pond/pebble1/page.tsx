7e9cc238ffe30c3352da03f13d4c2a3dac859883
lucynottonson <lucynottonson@icloud.com>
2025-08-27 14:22:43 -0700

    tx

--- app/pond/pebble1/page.tsx
+++ app/pond/pebble1/page.tsx
@@ -1,10 +1,7 @@
 export default function PondPage() {
   return (
     <div className="min-h-screen flex flex-col items-center p-2 gap-10">
-      <h1 className="text-2xl font-bold text-[var(--baccent)]">Pebble</h1>
-
-      <h1 className="text-2xl font-bold text-[var(--accent)]">DISCLAIMER</h1>
-
+      <h1 className="text-2xl font-bold text-[var(--baccent)]">DISCLAIMER</h1>
       <p>Any statement I make on this website, or in real life for that matter is not to be taken as a statement of certainty or objective fact. If I believed anything to be certainly or objectively true I would be stupid and wrong, and frankly I am offended by the idea of my thoughts and words being interpreted as declarations of certainty. I do not want to have to keep giving this disclaimer but for some reason everyone thinks that I deeply care about these topics and, even worse, that I believe myself to be of infinite wisdom. I do not know anything and have never claimed to know anything. I enjoy arguing and would like to be disagreed with. I do not even agree with myself most of the time. Generally speaking, I do not seriously care at all about any of these things, and simply think because I do not know what else to do. When I get better at coding and have the time, I will add a way for people to comment and disagree with me so that we can have discussions, and so that I can be wrong. When this happens we can all hold hands and marvel at the entirely pointless conclusions we have come to with our shared intellectual powers. Basically, do not take anything I say seriously ever. I am not taking it seriously. I am not a serious person and do not want to be considered one. 
 </p>
     </div>
