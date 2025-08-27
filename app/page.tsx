4896969263a7941ebb194aea8c3f8e756a529209
lucynottonson <lucynottonson@icloud.com>
2025-08-27 13:19:13 -0700

    im dumb

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,14 @@ export default function Home() {
         <section className="p-6 rounded bg-[var(--accent)] text-[var(--baccent)] shadow">
           <h2 className="text-2xl font-bold mb-2">Pebble World</h2>
           <p className="text-base">
-this is pebble world.          </p>
-          <p I do not know what this is or its purpose. In pond there are pebbles. If you want to see the current site colors, you can find them on colors. Throw is nothing. I'll change this when it is something. Info is also nothing. This is the only descriptive info you will recieve from me regarding the current status of pebble world.  </p>
+            this is pebble world.
+          </p>
+          <p>
+            I do not know what this is or its purpose. In pond there are pebbles. 
+            If you want to see the current site colors, you can find them on colors. 
+            Throw is nothing. I'll change this when it is something. Info is also nothing. 
+            This is the only descriptive info you will recieve from me regarding the current status of pebble world.
+          </p>
         </section>
       </main>
     </div>
