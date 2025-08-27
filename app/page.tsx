d5b0540f85f086102e1f25dbfc39cf666f69628f
lucynottonson <lucynottonson@icloud.com>
2025-08-27 13:01:37 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
           <h2 className="text-2xl font-bold mb-2">Pebble World</h2>
           <p className="text-base">
 this is pebble world.          </p>
+          <p I do not know what this is or its purpose. In pond there are pebbles. If you want to see the current site colors, you can find them on colors. Throw is nothing. I'll change this when it is something. Info is also nothing. This is the only descriptive info you will recieve from me regarding the current status of pebble world.  </p>
         </section>
       </main>
     </div>
