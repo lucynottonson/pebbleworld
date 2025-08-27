6484de70ff9ea622efd99f067430c90bb6ae6e46
lucynottonson <lucynottonson@icloud.com>
2025-08-27 13:03:32 -0700

    Update page.tsx

--- app/pond/pebble17/page.tsx
+++ app/pond/pebble17/page.tsx
@@ -2,7 +2,7 @@ export default function PondPage() {
   return (
     <div className="min-h-screen flex flex-col items-center p-2 gap-10">
       <h1 className="text-2xl font-bold text-[var(--baccent)]">Pebble</h1>
-      <p>This is a Pebble page.</p>
+      <p>Books I like</p>
     </div>
   );
 }
