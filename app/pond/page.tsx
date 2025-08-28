3c555cfdce19518a3b1b64a0a6896e1313c20c0a
lucynottonson <lucynottonson@icloud.com>
2025-08-28 13:04:30 -0700

    hi

--- app/pond/page.tsx
+++ app/pond/page.tsx
@@ -13,14 +13,11 @@ That should at least have something on it.
             <span className="text-[var(--baccent)] font-bold text-center break-words">Each pebble is an entirely seperate thing and none of them are connected and there is no universal styling or formatting across the different pebbles. 
 You may be thinking, &quot;this must be someone who does not care about systems of organization&quot; and unfortunately you would be wrong. 
 I care deeply about systems of organization to the point of not being able to do things at all if I have not decided on a system of organization beforehand. 
-There is one workaround to this paralysis I experience though, which is creating a system of organization that accommodates chaos. 
-This has solved a lot of my problems. 
-I know that to accept a project, there must be a system, and that the system must be consistent and perfect and the rules of the system must never be broken or the project will be abandoned. 
-This is almost impossible to actually do. There is only one workaround which is accepting a chaos system, or a constant improvement system, where there are a few consistent rules regarding the way the chaos is to be organized. As you may notice there are only six colors used on the entire site. This is the one rule that ties everything together. As long as that is consistent there are parameters and it is safe.      </span>
+There is one workaround to this paralysis I experience though, which is creating a system of organization that specifically accommodates chaos as part of its rules. 
           </div>
         </div>
         <div className="w-full border border-[var(--accent)] rounded-lg p-10 shadow-md flex flex-col items-center gap-4">
-          <span className="text-[var(--baccent)] font-bold">Many pebbles are currently blank, and many non-blank pebbles are unfinished. Although I have escaped not being able to continue something without an elaborate system, I have not escaped needing to create categories and blank placeholders before actually working on anything that goes inside them. This will come later. Maybe one day we can have an OCD exposures page and share our success with different ones. I stopped doing this like six months ago and decided that as long as I am not left to my own devices for too long this problem cannot get out of hand enough to warrant that type of intervention. Enjoy the pebbles. </span>
+          <span className="text-[var(--baccent)] font-bold">As you might notice, many pages are just placeholders. It is also the case that this is something I am constantly changing. Pebble world was started in May 2025 so any of this stuff could be from anywhere from then to the present day. I am only saying this because it is likely that I will think different things than things I say on here after time has passed, and will not update my opinion accordingly. As you guys probably know already, I change my mind a lot and can be convinced of anything. I will not put dates on any of this. Right now this site is in a state of chaos and I intend to eventually make it into a site of non-chaos. I do not know whether or not I will, but that is my intention.</span>
         </div>
       </div>
     </main>
