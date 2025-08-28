e0726eedb4d885f8ce454bb07511a85e8605d8d0
lucynottonson <lucynottonson@icloud.com>
2025-08-27 18:48:30 -0700

    q

--- app/pond/pebble11/page.tsx
+++ app/pond/pebble11/page.tsx
@@ -1,8 +1,48 @@
 export default function PondPage() {
   return (
-    <div className="min-h-screen flex flex-col items-center p-2 gap-10">
-      <h1 className="text-2xl font-bold text-[var(--baccent)]">Pebble</h1>
-      <p>This is a Pebble page.</p>
-    </div>
+    <main className="min-h-screen flex flex-col items-center p-2 gap-10">
+      <div className="w-full max-w-6xl flex flex-col items-center gap-2">
+
+        <div className="flex flex-col items-center gap-2 md:flex-row md:items-stretch w-full">
+          <div className="w-full border border-[var(--accent)] rounded-lg p-10 shadow-md flex flex-col items-center gap-4">
+             <span className="text-[var(--baccent)] font-bold">GOOD MORNING</span>
+           </div>
+           <div className="w-full border border-[var(--accent)] rounded-lg p-10 shadow-md flex flex-col items-center gap-4">
+             <span className="text-[var(--baccent)] font-bold">I have decided to add to pebble 11 pond page. Not sure when I will publish this or if I will remember to.</span>
+           </div>
+        </div>
+
+        <div className="w-full border border-[var(--accent)] rounded-lg p-10 shadow-md flex flex-col items-center gap-4">
+             <span className="text-[var(--baccent)] font-bold">I am very sorry to my lovely friends and family members who have yet to see a single positive or optimistic sentiment on this entire website and have been met with a lot of seemingly angry yapping. That was not my intention when I made this, it was just the easiest things to put on here as they are not very emotional and I find it easier to express myself when it comes from a place of slight indignation, although I know that this is bad. I have a lot of positive thoughts too. Do not worry about this. If I say somewhere else something that directly contradicts what I am saying here....... thats okay do not worry too much about it. They are both true. I am happy and sad. I don&rsquo;t think that anyone expected me to say anything incredibly peace loving or optimistic in the first place but whatever.</span>
+        </div>
+
+        <div className="w-full border border-[var(--accent)] rounded-lg p-10 shadow-md flex flex-col items-center gap-4">
+             <span className="text-[var(--baccent)] font-bold">Anyway, there are many reasons to be happy, just as there are many reasons to be sad. We are falling into fascism in America! Who can be happy about that? Apparently many people, but I forgive them and you should too, because they are misinformed and even if they are genuinely stupid we need them all. We all need each other. I saw a really lovely video pair of videos on instagram reels which encouraged its viewers to, in the comment section, say something positive about their political counterpart. The comments were full of similar sentiments from both sides. We all want the same things. Everyone believes in fairness. We all want justice. And we all have recieved information that is intentionally deceptive and has led us to believe that we want different things, and that we should fight each other over it. I do not live by this, and I am sorry for it, but sometimes I cannot restrain myself. This is a personal flaw of mine that I am working on.</span>
+        </div>
+
+        <div className="flex flex-col items-center gap-2 md:flex-row md:items-stretch w-full">
+          <div className="w-full border border-[var(--accent)] rounded-lg p-10 shadow-md flex flex-col items-center gap-4">
+             <span className="text-[var(--baccent)] font-bold"> I thought when I made this website that I would have a lot more things to say, but despite having too many things to say most of the time, when I ask myself what things I have to say, nothing presents itself to me. I don&rsquo;t know any of my thoughts and I couldn&rsquo;t pull them from my mind on command. I am not sure how people do that. </span>
+           </div>
+           <div className="w-full border border-[var(--accent)] rounded-lg p-10 shadow-md flex flex-col items-center gap-4">
+             <span className="text-[var(--baccent)] font-bold">I have been recently very fascinated by how other people understand their brain. I recently found out that not everyone conceptualizes their thoughts as being in any sort of brain &rsquo;space&rsquo; or conceptualizes their thoughts as being any sort of thing in the first place. I am very eager to create a comment section so that I can hear what everyone sees their thoughts as. All of my thoughts are in pictures, often shapes that represent the concepts I am thinking about. I have different &rsquo;locations&rsquo; in my brain that I visit while I am thinking. Concepts laid out in numbers have representational graphs that I can sort of float around and see from different angles depending on the context. This I know isn&rsquo;t typical, I believe in psych stuff it is called space-time synesthesia. By the way, I do not have spellcheck and am typing this directly into the console, so if I misspell something I will not know. I am saying that after writing the word &rsquo;synesthesia&rsquo; which looks kind of right but could easily be wrong. What I am writing also fades off screen fairly quickly as this is all on one line. I could start a new one but it&rsquo;s sort of fun to not remember what I am typing. It&rsquo;s sort of like a stream of consiousness exercise.</span>
+          </div>
+        </div>
+
+         <div className="w-full border border-[var(--accent)] rounded-lg p-10 shadow-md flex flex-col items-center gap-4">
+             <span className="text-[var(--baccent)] font-bold">I started a new line anyway. I got bored of that one. Anyway, I am curious to know how other people think and many people have many answers and they seem to all be different. This is lovely, but I wonder why? It makes less sense for the architecture of our thoughts to be so different from one another than if they were all to be the same. I have talked to some people who believe that all of our experience can technically be boiled down to neuron firings and whatnot, even if not currently possible, and I just don&rsquo;t think this can be true? There surely are things that cannot be described accurately or completely in this manor? I believe in science, and I love science. I just don&rsquo;t think science is everything. What about phenomenology? You can&rsquo;t tell from neurons what it actually is like to be another person and isn&rsquo;t that relevant? Isn&rsquo;t that part of it? I just can&rsquo;t believe that&rsquo;s all there is.</span>
+        </div>
+
+        <div className="flex flex-col items-center gap-2 md:flex-row md:items-stretch w-full">
+          <div className="w-full border border-[var(--accent)] rounded-lg p-10 shadow-md flex flex-col items-center gap-4">
+             <span className="text-[var(--baccent)] font-bold">I am starting a new line again. I just want to fill in all the placeholders. I might add to this one but maybe not. I have been thinking a lot about this but I still can&rsquo;t really decide on anything conclusive. It sort of bothers me to think of a world where all of our experiences could be reduced to neuron firings although I suppose it isn&rsquo;t entirely impossible. It just feels sort of bleak and horrible. If that were possible I think I would love the data, and there would be endless data and so many things to interpret. I could look into other people&rsquo;s brains and know once and for all what it was like to not have mine. I could have validation that the way I see the world is either insane or not insane. I would appreciate that. But I am afraid for several reasons, as I think we all should be and probably all are.</span>
+           </div>
+           <div className="w-full border border-[var(--accent)] rounded-lg p-10 shadow-md flex flex-col items-center gap-4">
+             <span className="text-[var(--baccent)] font-bold">Maybe others don&rsquo;t care as much about how people think, and that&rsquo;s part of it. But I care and think a lot about how people are thinking, not just systematically but their actual experience. Could neuron firings really tell us how someone understands time? Could my calendar really be translated out of my head based on data and recreated on a computer screen? I have always wanted that but at the same time I don&rsquo;t know if it would be right. It would take something away from us, and for me at least I think it would make me crazy? Maybe those are the answers we really shouldn&rsquo;t have. I&rsquo;m not even really sure what I&rsquo;m getting at here but the more I imagine this reality the harder of a time I have believing in its legitimacy. There is just no way this could be true. There is clearly something more. But is that not a religious belief? Is that not just a feeling? Calculus can draw anything, why couldn&rsquo;t it draw and interpret my calendar circle? But this idea assumes people think in a translatable way. We can&rsquo;t even, in language, describe our thoughts to someone else. You would have to be able to map out all possible thoughts before you were able to interpret data regarding people&rsquo;s thoughts. And that alone seems impossible, right? I don&rsquo;t know. This question keeps bothering me.</span>
+          </div>
+        </div>
+
+      </div>
+    </main>
   );
 }
