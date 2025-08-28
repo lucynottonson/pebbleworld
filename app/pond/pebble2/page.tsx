5f9ae86f4b68de0bfaeda1b3794890fa04b7983a
lucynottonson <lucynottonson@icloud.com>
2025-08-27 18:49:40 -0700

    add

--- app/pond/pebble2/page.tsx
+++ app/pond/pebble2/page.tsx
@@ -2,7 +2,7 @@ export default function PondPage() {
   return (
     <div className="min-h-screen flex flex-col items-center p-2 gap-10">
       <h1 className="text-2xl font-bold text-[var(--baccent)]">Pebble</h1>
-      <p>This is a Pebble page.</p>
+      <p>A lot of these pages are blank. I am trying to update the sidebar so that non blank pages have a title, but I have not done all of them. There are a bunch of pages filled out with things that are unlabeled.</p>
     </div>
   );
 }
