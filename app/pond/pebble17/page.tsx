97e590d2f16151404a8f774243e06a800be9c40a
lucynottonson <lucynottonson@icloud.com>
2025-08-28 02:30:59 -0700

    Update page.tsx I’m fucking stupid

--- app/pond/pebble17/page.tsx
+++ app/pond/pebble17/page.tsx
@@ -1,5 +1,3 @@
-
-
 export default function PondPage() {
   return (
     <main className="min-h-screen flex flex-col items-center p-2 gap-10">
@@ -11,7 +9,7 @@ export default function PondPage() {
 
         <div className="w-full border border-[var(--accent)] rounded-lg p-10 shadow-md flex flex-col items-center gap-4">
           <span className="text-[var(--baccent)] font-bold">
-            here is a list of books that have critically shaped my existence as a thinking person. I am going to be very particular about what ends up on this list. I have enjoyed many books but not all of them have critically shaped my existence as a thinking person, obviously. Also they are not in chronological order or any order other than the one I put them in arbitrarily.
+            {`here is a list of books that have critically shaped my existence as a thinking person. I am going to be very particular about what ends up on this list. I have enjoyed many books but not all of them have critically shaped my existence as a thinking person, obviously. Also they are not in chronological order or any order other than the one I put them in arbitrarily.`}
           </span>
         </div>
 
@@ -20,26 +18,27 @@ export default function PondPage() {
             Iris - Herman Hesse
           </span>
           <p className="text-[var(--text)] text-sm leading-relaxed">
-            Not actually a book, one of Herman Hesse’s short stories. You kinda just have to read it and you should. Had sort of a spiritual experience reading this for some reason. 
+            {`Not actually a book, one of Herman Hesse’s short stories. You kinda just have to read it and you should. Had sort of a spiritual experience reading this for some reason.`}
           </p>
         </div>
 
         <div className="w-full border border-[var(--accent)] rounded-lg p-6 shadow-md flex flex-col gap-2">
           <span className="text-[var(--baccent)] font-bold text-lg">
-            Brave New World - Aldous Huxley 
+            Brave New World - Aldous Huxley
           </span>
           <p className="text-[var(--text)] text-sm leading-relaxed">
-This is a flawed book in many ways, and in terms of dystopian novels I think 1984 is far better. I read this in high school long before I read 1984 and it sort of radicalized me in the way I've heard others describe 1984 radicalized them. By the time I read 1984 I had already developed an understanding of government corruption and it sort of just put words and a story to what I already believed. I actually still think our dystopian future will look far more similar to Brave New World than 1984 though. Consumerism is a much more pervasive and disturbing mechanism of capitalism than propaganda or surveillance, although they are of course all connected.
-I will probably add more to this one later but I haven't revisited it in a while. It made me much more mindful of both my own consumption and the information that was constantly being funneled into my brain from various sources. Unfortunately it did also trigger the development of an ocd behavior where I had to play positive affirmations while I slept to be as productive as possible with my time. Thankfully I grew out of that.
+            {`This is a flawed book in many ways, and in terms of dystopian novels I think 1984 is far better. I read this in high school long before I read 1984 and it sort of radicalized me in the way I've heard others describe 1984 radicalized them. By the time I read 1984 I had already developed an understanding of government corruption and it sort of just put words and a story to what I already believed. I actually still think our dystopian future will look far more similar to Brave New World than 1984 though. Consumerism is a much more pervasive and disturbing mechanism of capitalism than propaganda or surveillance, although they are of course all connected.
+            
+            I will probably add more to this one later but I haven't revisited it in a while. It made me much more mindful of both my own consumption and the information that was constantly being funneled into my brain from various sources. Unfortunately it did also trigger the development of an ocd behavior where I had to play positive affirmations while I slept to be as productive as possible with my time. Thankfully I grew out of that.`}
           </p>
         </div>
 
         <div className="w-full border border-[var(--accent)] rounded-lg p-6 shadow-md flex flex-col gap-2">
           <span className="text-[var(--baccent)] font-bold text-lg">
-            Cat’s Cradle - Kurt Vonnegut 
+            Cat’s Cradle - Kurt Vonnegut
           </span>
           <p className="text-[var(--text)] text-sm leading-relaxed">
-I’ve probably talked to everyone I know about this book. It’s not the most amazing book in the world but, similarly to Master and Margarita, it soothed my existential inflictions both while I was reading it and for a short period after. Vonnegut always does this but since this one addressed the world ending it was particularly nice. Great absurdist lens of humanity and science and religion. I would recommend this to anyone who hasn’t read any Vonnegut, it’s easy and short and not too depressing. 
+            {`I’ve probably talked to everyone I know about this book. It’s not the most amazing book in the world but, similarly to Master and Margarita, it soothed my existential inflictions both while I was reading it and for a short period after. Vonnegut always does this but since this one addressed the world ending it was particularly nice. Great absurdist lens of humanity and science and religion. I would recommend this to anyone who hasn’t read any Vonnegut, it’s easy and short and not too depressing.`}
           </p>
         </div>
 
@@ -48,16 +47,16 @@ I’ve probably talked to everyone I know about this book. It’s not the most a
             The Master and Margarita - Mikhail Bulgakov
           </span>
           <p className="text-[var(--text)] text-sm leading-relaxed">
-I read the Burgin O’Connor translation and I thought it was good, but I also have the Alpin one and intend to read it too. I’ll let you know what I think once I’ve read both. This is one of the best books I’ve ever read and I think it is probably to date the most significant to me. It is beautifully written and a perfect length and approaches the concept of evil with such whimsy that it doesn’t even really feel like evil anymore. It also helped me resolve some of my ocd problems which I don’t feel like getting into although they came back quickly. I can still conjure the feeling of reading this though and it helps a little. The whole story felt very real to me somehow. I will make my children read this if I have them. It feels more like a fairytale than a story about the devil. And it has such a beautiful female rage scene. If I don’t abandon myself and go to grad school I will learn Russian for this book (and for Dostoyevsky as well). At the moment it’s not looking good for that life plan but there must be an answer. There’s an answer somewhere, right guys? I’m running out of time. I have so much more to say about this book but am having trouble articulating it. 
+            {`I read the Burgin O’Connor translation and I thought it was good, but I also have the Alpin one and intend to read it too. I’ll let you know what I think once I’ve read both. This is one of the best books I’ve ever read and I think it is probably to date the most significant to me. It is beautifully written and a perfect length and approaches the concept of evil with such whimsy that it doesn’t even really feel like evil anymore. It also helped me resolve some of my ocd problems which I don’t feel like getting into although they came back quickly. I can still conjure the feeling of reading this though and it helps a little. The whole story felt very real to me somehow. I will make my children read this if I have them. It feels more like a fairytale than a story about the devil. And it has such a beautiful female rage scene. If I don’t abandon myself and go to grad school I will learn Russian for this book (and for Dostoyevsky as well). At the moment it’s not looking good for that life plan but there must be an answer. There’s an answer somewhere, right guys? I’m running out of time. I have so much more to say about this book but am having trouble articulating it.`}
           </p>
         </div>
 
         <div className="w-full border border-[var(--accent)] rounded-lg p-6 shadow-md flex flex-col gap-2">
           <span className="text-[var(--baccent)] font-bold text-lg">
-Equus - Peter Shaffer
+            Equus - Peter Shaffer
           </span>
           <p className="text-[var(--text)] text-sm leading-relaxed">
-This is also not a book, but the word books is used loosely here. Words on pages. This is a play I read in high school for a class and then again in college for a class and for some reason no one else has read it. Although like everything else from this time period it reeks of Freud, it is brilliant and insane. You sort of just have to read it, sorry. I am noticing I’m actually terrible at summarizing why any of these are significant. It’s really just a list. This is kind of pointless although I hope I can just use it to keep a log for when I have more because I’ll obviously become a better and changed person after I read more things. The answers have to be somewhere right guys? Someone wrote them down and I just have to find them! We just have to find where someone wrote it all down! 
+            {`This is also not a book, but the word books is used loosely here. Words on pages. This is a play I read in high school for a class and then again in college for a class and for some reason no one else has read it. Although like everything else from this time period it reeks of Freud, it is brilliant and insane. You sort of just have to read it, sorry. I am noticing I’m actually terrible at summarizing why any of these are significant. It’s really just a list. This is kind of pointless although I hope I can just use it to keep a log for when I have more because I’ll obviously become a better and changed person after I read more things. The answers have to be somewhere right guys? Someone wrote them down and I just have to find them! We just have to find where someone wrote it all down!`}
           </p>
         </div>
 
@@ -66,13 +65,13 @@ This is also not a book, but the word books is used loosely here. Words on pages
             Flowers for Algernon - Daniel Keyes
           </span>
           <p className="text-[var(--text)] text-sm leading-relaxed">
-I find it odd that they give a heavily censored version this book to middle schoolers. I read this in middle school, and upon finding out that we did not read the original version I promptly got a copy of it and read it in its entirety. I found out at that point why parts of it had been removed from our assigned reading, but it wasn’t too bad. This was my first exposure to something that felt like it spoke to my experience, so it was significant to me. I didn’t know what autism was at this time, and was, in most ways, a 12 year old narcissist. I had not failed at anything yet and people didn’t like me because I was weird. I did what every other little autistic kid did and overidentified with my intelligence and in turn isolated myself even more. This book sort of made me begin to understand that dynamic between me and others. It made me angry too, but I was already quite angry at this time, to be fair. What made me add this to the list was the fact that I read it again a little over a year ago and was surprised to find that it impacted me in exactly the same way as it had in middle school. I don’t have basically anything in common with my 12 year old self, but apparently something in here triggered something in both of us. I obviously still fight my solipsism demons daily but I think this book triggers something more than just my awareness of that fact. Charlie doesn’t get to be treated like a human being whether he’s intelligent or not. I guess it’s the visceral descriptions of shame for simply existing at all that I could relate to. The idea of savantism is just as dehumanizing as declaring that an autistic child is incapable of doing anything at all and shouldn’t be allowed in normal society.  This book also reeks of Freud but it’s tasteful. It’s kind of sweet how everyone was excited about Freud. I can imagine it added a lot of dimension to the act of developing a character and it was probably a lot of fun to make a subconscious experience and have a template for that. 
+            {`I find it odd that they give a heavily censored version this book to middle schoolers. I read this in middle school, and upon finding out that we did not read the original version I promptly got a copy of it and read it in its entirety. I found out at that point why parts of it had been removed from our assigned reading, but it wasn’t too bad. This was my first exposure to something that felt like it spoke to my experience, so it was significant to me. I didn’t know what autism was at this time, and was, in most ways, a 12 year old narcissist. I had not failed at anything yet and people didn’t like me because I was weird. I did what every other little autistic kid did and overidentified with my intelligence and in turn isolated myself even more. This book sort of made me begin to understand that dynamic between me and others. It made me angry too, but I was already quite angry at this time, to be fair. What made me add this to the list was the fact that I read it again a little over a year ago and was surprised to find that it impacted me in exactly the same way as it had in middle school. I don’t have basically anything in common with my 12 year old self, but apparently something in here triggered something in both of us. I obviously still fight my solipsism demons daily but I think this book triggers something more than just my awareness of that fact. Charlie doesn’t get to be treated like a human being whether he’s intelligent or not. I guess it’s the visceral descriptions of shame for simply existing at all that I could relate to. The idea of savantism is just as dehumanizing as declaring that an autistic child is incapable of doing anything at all and shouldn’t be allowed in normal society. This book also reeks of Freud but it’s tasteful. It’s kind of sweet how everyone was excited about Freud. I can imagine it added a lot of dimension to the act of developing a character and it was probably a lot of fun to make a subconscious experience and have a template for that.`}
           </p>
         </div>
 
         <div className="w-full border border-[var(--accent)] rounded-lg p-6 shadow-md flex flex-col gap-2">
           <p className="text-[var(--text)] text-sm leading-relaxed">
-That’s all for right now. I guess the order is basically whatever is most significant to me enough to remember when conjuring a list like this. Anything else added maybe wasn’t as significant since it didn’t come to mind. But maybe that’s not true. 
+            {`That’s all for right now. I guess the order is basically whatever is most significant to me enough to remember when conjuring a list like this. Anything else added maybe wasn’t as significant since it didn’t come to mind. But maybe that’s not true.`}
           </p>
         </div>
 
