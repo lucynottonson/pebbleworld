15aab4f1211c1ce1cddfbf13620f358d2b13a055
lucynottonson <lucynottonson@icloud.com>
2025-08-27 17:01:46 -0700

    Update page.tsx

--- app/pond/pebble17/page.tsx
+++ app/pond/pebble17/page.tsx
@@ -15,7 +15,7 @@ export default function PondPage() {
    <div className="w-full border border-[var(--accent)] rounded-lg p-10 shadow-md flex flex-col items-center gap-4">
              <span className="text-[var(--baccent)] font-bold">Brave new world 
 This is a flawed book in many ways, and in terms of dystopian novels I think 1984 is far better. I read this in high school long before I read 1984 and it sort of radicalized me in the way I’ve heard others describe 1984 radicalized them. By the time I read 1984 I had already developed an understanding of government corruption and it sort of just put words and a story to what I already believed. I actually still think our dystopian future will look far more similar to Brave New World than 1984 though. Consumerism is a much more pervasive and disturbing mechanism of capitalism than propaganda or surveillance, although they are of course all connected. I will probably add more to this one later but I haven’t revisited it in a while. It made me much more mindful of both my own consumption and the information that was constantly being funneled into my brain from various sources. Unfortunately it did also trigger the development of an ocd behavior where I had to play positive affirmations while I slept to be as productive as possible with my time, but thankfully I grew out of that. </span>
-        </div>
+       I cannot add more im on my ohone and this id impossible to code while on my pjone. all of the test of the books will be added later.  </div>
               
       </div>
     </main>
