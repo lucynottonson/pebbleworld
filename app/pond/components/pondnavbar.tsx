052f1842b61b75dc73aa6a78d2de4617b2be2d3f
lucynottonson <lucynottonson@icloud.com>
2025-08-28 12:40:33 -0700

    nav

--- app/pond/components/pondnavbar.tsx
+++ app/pond/components/pondnavbar.tsx
@@ -46,7 +46,7 @@ export default function Sidebar() {
               : "hover:bg-white hover:text-[var(--accent)]"
           }`}
         >
-          Pebble 3 - blank
+          Links to other things I have made
         </a>
           <a
           href="/pond/pebble4"
