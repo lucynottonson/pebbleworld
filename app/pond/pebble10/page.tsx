d8b824727aa2802509956905d124aeac36236cff
lucynottonson <lucynottonson@icloud.com>
2025-08-28 21:08:31 -0700

    Update page.tsx

--- app/pond/pebble10/page.tsx
+++ app/pond/pebble10/page.tsx
@@ -1,8 +1,13 @@
 export default function PondPage() {
   return (
     <div className="min-h-screen flex flex-col items-center p-2 gap-10">
-      <h1 className="text-2xl font-bold text-[var(--baccent)]">Pebble</h1>
-      <p>This is a Pebble page.</p>
+      <h1 className="text-2xl font-bold text-[var(--baccent)]">Anti-solipsism rules! </h1>
+      <p>this is my personal list of anti-solipsism rules (or affirmations, or whatever you would like to refer to them as). They designed to save grown up savant toddlers from being insufferable people later in life. reform is possible. it is also necessary if you want to be happy. </p>
+<ul>
+<li>1. everyone you meet knows more than you about at least one thing, but probably many things. </li>
+<li>2. you do not know anything, and you are probably at least partially wrong about most of the things you are currently certain of.</li>
+<li>3. intelligence is not an inherently positive quality.</li>
+<li>4. there are people like you. there are almost 8 billion people on earth and many of them are exactly like you.</li>
     </div>
   );
 }
