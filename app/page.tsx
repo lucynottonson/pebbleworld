58bf1ebd65df6af07b3b080ae1c2405067f776af
lucynottonson <lucynottonson@icloud.com>
2025-08-28 20:29:28 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,11 @@ export default function Home() {
         <section className="p-6 rounded bg-[var(--accent)] text-[var(--baccent)] shadow">
           <h2 className="text-2xl font-bold mb-2">Pebble World</h2>
           <p className="text-base">
-            this is pebble world.
+            PEBBLE WORLD IS BACK
           </p>
           <p>
-            I do not know what this is or its purpose. In pond there are pebbles. Pebbles are just me yapping in various ways about various things. It is mostly disorganized. 
-            If you want to see the current site colors, you can find them on colors. 
-            Throw is nothing. I will change this when it is something. Info is also nothing. 
-            This is the only descriptive info you will recieve from me regarding the current status of pebble world.
+           Good news guys.  
+          Google form will be up when I have time. I am very excited to make big data! for now, pond is up, and it is full of old stuff and some new stuff. I promise I will edit and organize soon. colors page should be working and throw and info are both blank. Any updates will be here and not on info (because it is blank). 
           </p>
         </section>
       </main>
