fa3a17fd2422d312f3edf235575c41f8361688ec
lucynottonson <lucynottonson@icloud.com>
2025-08-28 21:41:03 -0700

    Update page.tsx

--- app/pond/pebble10/page.tsx
+++ app/pond/pebble10/page.tsx
@@ -2,12 +2,22 @@ export default function PondPage() {
   return (
     <div className="min-h-screen flex flex-col items-center p-2 gap-10">
       <h1 className="text-2xl font-bold text-[var(--baccent)]">Anti-solipsism rules! </h1>
-      <p>this is my personal list of anti-solipsism rules (or affirmations, or whatever you would like to refer to them as). They designed to save grown up savant toddlers from being insufferable people later in life. reform is possible. it is also necessary if you want to be happy. </p>
+      <p>this is my personal list of anti-solipsism rules (or affirmations, or whatever you would like to refer to them as). They are designed to save grown up savant toddlers from being insufferable people later in life. reform is possible. it is also necessary if you want to be happy and normal. </p>
 <ul>
 <li>1. everyone you meet knows more than you about at least one thing, but probably many things. </li>
 <li>2. you do not know anything, and you are probably at least partially wrong about most of the things you are currently certain of.</li>
 <li>3. intelligence is not an inherently positive quality.</li>
 <li>4. there are people like you. there are almost 8 billion people on earth and many of them are exactly like you.</li>
+<li>5. you do not need to talk basically ever and no one cares what you are thinking.</li>
+<li>6. you should constantly be aware of your negative qualities and be working to fix them.</li>
+<li>7. you are bad at everything you think you are good at.</li>
+<li>8. assume your thoughts are the least important in the room at all times.</li>
+<li>9. you have no additional responsibility to do anything for the world or other people. even if someone told you that.</li>
+<li>10. it is not mature or positive in any way to take everything extremely seriously. nothing is serious. and thats an annoying quality. everyone who is being less serious than you is happier than you.</li>
+<li>11. being negative is not a sign of intelligence. people who are positive are not stupider than you. consider that you are just negative and bitter.</li>
+<li>12. you do not have a real identity and no one cares who you are or what qualities you have. nothing you do matters any more than anything anyone else does, in fact it should matter less.</li>
+<li>13. go outside and look at trees and flowers and be happy about their existence. do things that children do. you are not serious. do you want to die knowing you took yourself this seriously for your entire life?</li>
+<li> NOT DONE. WILL ADD MORE LATER </li>
     </div>
   );
 }
