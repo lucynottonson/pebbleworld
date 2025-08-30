7c30c9194cc11aaaa539e252ec4b7972e630126c
lucynottonson <lucynottonson@icloud.com>
2025-08-29 18:32:25 -0700

    Update page.tsx

--- app/pond/page.tsx
+++ app/pond/page.tsx
@@ -8,7 +8,7 @@ export default function PondPage() {
         <div className="flex flex-col items-center gap-2 md:flex-row md:items-stretch w-full">
           <div className="flex-grow min-w-10 border border-[var(--accent)] rounded-lg p-6 shadow-md flex flex-col items-center gap-2 bg-white/70">
             <span className="text-black font-bold text-center break-words">
-              This is pond. This section of pebble world is literally just me rambling and random lists and data I have collected. It is basically my notes app but only the notes I have decided could be helpful to anyone. I will likely change pond a lot but it was a lot more chaotic before now so keep that in mind while you are looking at it. 
+              This is pond. This section of pebble world is literally just me rambling and random lists and data I have collected. It is basically my notes app but only the notes I have decided could be helpful to anyone. I will likely change pond a lot but it was a lot more chaotic before now so keep that in mind while you are looking at it. I made the backgrounf for this page on procreate like 5 minutes ago on my phone. it kind of looks like real water. not a pond maybe but who cares.
             </span>
           </div>
 
