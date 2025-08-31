a566ab1e0a1d2e5b88c7b96ecabbd40b704b98a0
lucynottonson <lucynottonson@icloud.com>
2025-08-30 18:35:02 -0700

    QUESTION

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,21 @@ export default function Home() {
           </p>
           <p>
            Good news guys.  
-          Google form will be up when I have time. I am very excited to make big data! for now, pond is up, and it is full of old stuff and some new stuff. I promise I will edit and organize soon. colors page should be working and throw and info are both blank. Any updates will be here and not on info (because it is blank). 
+          Pond is up, and it is full of old stuff and some new stuff. I promise I will edit and organize soon. colors page should be working and throw and info are both blank. Any updates will be here and not on info (because it is blank). 
           </p>
+          <p>More importantly,</p>
+          <h2>THE THINKING STYLES GOOGLE FORM IS READY!!!!!!</h2>
+          <div className="flex flex-col gap-1">
+     <a
+     href="https://docs.google.com/forms/d/e/1FAIpQLSeh_q9xUInvzhbid-OJ53mKrZlz8J0svofIlH7tBnG9eNi0qg/viewform?usp=header"
+     target="_blank"
+     rel="noopener noreferrer"
+      className="w-full px-4 py-2 bg-[var(--accent)] text-[var(--baccent)] font-semibold rounded shadow hover:opacity-80 transition text-center">
+     PLEASE GIVE ME DATA
+     </a>
+     <p className="text-sm text-[var(--text)]">
+questionnaire that asks you about how you think. I cut it down a lot so hopefully it will not take very long to complete. Please give me access to your brains! There is also a space at the end for you to put a form of contact so I can share the data with you one day.</p>
+  </div>
         </section>
       </main>
     </div>
