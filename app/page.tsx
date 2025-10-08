6efccd38666626844c4540b30db7523acfe60ac0
lucynottonson <lucynottonson@icloud.com>
2025-10-07 18:53:13 -0700

    h

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function HomePage() {
 
       <div className="flex flex-col items-center justify-center h-full space-y-6">
        <div className="w-3/4 max-w-2xl h-32 bg-[var(--text)]/10 border border-[var(--background)] rounded-xl flex items-center justify-center">
-        <p className={`text-[var(--text)] ${cherryBombOne.className}`}>HAPPY SEPTEMBER!!! School has started, and now life is worth living. Any nihilistic or otherwise anti-fun statement made over the summer is no longer part of my worldview. I changed the site colors to reflect this shift. Sorry about that. I get unhappy when bored. Never take anything I say seriously.</p>
+        <p className={`text-[var(--text)] ${cherryBombOne.className}`}>HAPPY OCTOBER!!! School has started, and now life is worth living. Any nihilistic or otherwise anti-fun statement made over the summer is no longer part of my worldview. I changed the site colors to reflect this shift. Sorry about that. I get unhappy when bored. Never take anything I say seriously.</p>
       </div>
       <div className="w-3/4 max-w-xl h-32 bg-[var(--text)]/10 border border-[var(--background)] rounded-xl flex items-center justify-center">
         <p className={`text-[var(--text)] ${cherryBombOne.className}`}>I am currently trying to create an organization system that is normal and makes sense. This is more difficult than you might think. Most of the site is down right now because of this. You cannot see the progress that has been made, but I promise it is coming together.</p> 
